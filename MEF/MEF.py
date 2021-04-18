7ca9ed9e82035791a06843613d6f0e805a5ec80d
João Victor <joaovitormunizlopes@gmail.com>
2021-04-17 22:21:33 -0300

    Functional to Class development

--- MEF/MEF.py
+++ MEF/MEF.py
@@ -11,18 +11,39 @@ def acceptor(valids):
             resp = inquirer.prompt(questions)
             return resp
 
-def transition_function(initialState,finalState,transitions):
-    return
+def transition_function(initialState,finalState,transitions,output):
+    outputString = ''
+    resp = initial_state
+    exit_state = finalState 
+
+    while resp!=exit_state:
+        response = transitions[transitions[:,0]==resp][0,1:]
+        resp = acceptor(response)['states']
+        outputString += str(output[output[:,0]==resp][0,1:][0])
+    
+    return outputString
     
 class Finite_State_Machine:
-    def __init__(states,inputs,initialState,finalState,transition_function):
+    def __init__(self,states,initial_state,final_state,transition_function):
         self.transition_function = transition_function
-        self.initialState = initialState
-        self.finalState = finalState
-        self.inputs = inputs
+        self.initial_state = initial_state
+        self.final_state = final_state
         self.states = states 
+    
+    def execute(self):
+        states = self.states.iloc[:,0].values
+        transitions = self.states.iloc[:,0:3].values
+        output = self.states.iloc[:,0:4:3].values
+        response = self.transition_function(self.initial_state,self.final_state,transitions,output)
+        print(response)
+
 
 if __name__=="__main__":
     # Uma matriz nx3 que representa a descrição da tabela de estado de tal máquina
-    machine = pd.read_excel('MEF.ods', engine='odf')
-    print(machine)
+    states = pd.read_excel('MEF.ods', engine='odf')
+
+    initial_state = input('Initial state:')
+    exit_state = input('Exit State:')
+
+    MEF = Finite_State_Machine(states,initial_state,exit_state,transition_function)
+    MEF.execute()
