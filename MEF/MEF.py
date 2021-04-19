cf50616d23168596e66fbe74645cc4f1d598a49f
João Victor <joaovitormunizlopes@gmail.com>
2021-04-19 20:26:03 -0300

    Using class State in MEF

--- MEF/MEF.py
+++ MEF/MEF.py
@@ -1,32 +1,39 @@
+
 import pandas as pd
 import inquirer
 
-def acceptor(valids,input_pop):
-    ''' Acceptor style finite state machine for user input '''
-    if not len(valids) is not 0:
-        return 'Parece que chegamos ao estado final'
-    return str(list(valids)[int(input_pop)])
+class State:
+    def __init__(self,state,input0,input1,output):
+        self.state = state
+        self.output = output
+        self.transitions = [input0,input1]
 
-def transition_function(initial_state,input_string,transitions,output):
-    outputString = ''
-    resp = initial_state
-
-    str_array = list(input_string)
-
-    #while  resp!=exit_state
-    while len(str_array)>0:
-        input_pop = str_array.pop(0)
-        response = transitions[transitions[:,0]==resp][0,1:]
-        resp = acceptor(response,input_pop)
-        outputString += str(output[output[:,0]==resp][0,1:][0])
-    
-    return outputString
-    
 class Finite_State_Machine:
-    def __init__(self,states,transition_function):
-        self.states = states 
-        self.transition_function = transition_function
+    def __init__(self,states):
+        self.states = [] 
+        for row in states.iterrows():
+            state = State(row[1]['estado'],[0,row[1][0]],[1,row[1][1]],row[1]['output'])
+            self.states.append(state)
 
+    def acceptor(self,valids,input_pop):
+        if not len(valids) is not 0:
+            return 'Parece que chegamos ao estado final'
+        return str(list(valids)[int(input_pop)][1])
+
+    def transition_function(self):
+
+        outputString = ''
+        str_array = list(self.input_string)
+        resp = self.initial_state.state
+
+        while len(str_array)>0:
+            input_pop = str_array.pop(0)
+            response =  self.states[[i.state for i in self.states].index(resp)]
+            resp = self.acceptor(response.transitions,input_pop)
+            outputString += str(response.output)
+        
+        return outputString
+            
     def start(self): 
         main=True
         options = [['Sim',True],['Não',False]]
@@ -36,22 +43,28 @@ class Finite_State_Machine:
             
             if not main:
                 break
-            
-            self.initial_state = input('Initial state:')
+    
+            self.initial_state =  self.states[[i.state for i in self.states].index(input('Initial state:'))]    
             self.input_string = input('Input string:')
             self.execute()
     
+
     def execute(self):
-        states = self.states.iloc[:,0].values
-        transitions = self.states.iloc[:,0:3].values
-        output = self.states.iloc[:,0:4:3].values
-        response = self.transition_function(self.initial_state,self.input_string,transitions,output)
+        response = self.transition_function()
         print("Output: ",response)
 
 
 if __name__=="__main__":    
+    '''
+    S: 
+        States
+        Alfa
+        Outputs
+
+    M: (S,input_string,alfa,initial_state,exit_state,transitions)
+    M: (S,transitions)
+    '''
     # Uma matriz nx3 que representa a descrição da tabela de estado de tal máquina
     states = pd.read_excel('MEF.ods', engine='odf')
-    #M: (S,input_string,alfa,initial_state,exit_state,transitions)
-    MEF = Finite_State_Machine(states,transition_function)
-    MEF.start()    
+    MEF = Finite_State_Machine(states)
+    MEF.start() 
