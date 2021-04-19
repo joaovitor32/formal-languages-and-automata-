6d55d7b06c34a9433ae0c0081f11f4601cb04bac
João Victor <joaovitormunizlopes@gmail.com>
2021-04-18 22:23:12 -0300

    Updating MEF machine

--- MEF/MEF.py
+++ MEF/MEF.py
@@ -7,7 +7,7 @@ def acceptor(valids,input_pop):
         return 'Parece que chegamos ao estado final'
     return str(list(valids)[int(input_pop)])
 
-def transition_function(initialState,input_string,transitions,output):
+def transition_function(initial_state,input_string,transitions,output):
     outputString = ''
     resp = initial_state
 
@@ -23,12 +23,24 @@ def transition_function(initialState,input_string,transitions,output):
     return outputString
     
 class Finite_State_Machine:
-    def __init__(self,states,input_string,initial_state,transition_function):
+    def __init__(self,states,transition_function):
         self.states = states 
-        self.input_string = input_string
-        self.initial_state = initial_state
         self.transition_function = transition_function
 
+    def start(self): 
+        main=True
+        options = [['Sim',True],['Não',False]]
+        while main:
+            question = [inquirer.List('prompt',message="Deseja continuar?",choices=[options[0][0],options[1][0]])]
+            main = options[[i[0] for i in options].index(inquirer.prompt(question)['prompt'])][1]
+            
+            if not main:
+                break
+            
+            self.initial_state = input('Initial state:')
+            self.input_string = input('Input string:')
+            self.execute()
+    
     def execute(self):
         states = self.states.iloc[:,0].values
         transitions = self.states.iloc[:,0:3].values
@@ -40,20 +52,6 @@ class Finite_State_Machine:
 if __name__=="__main__":    
     # Uma matriz nx3 que representa a descrição da tabela de estado de tal máquina
     states = pd.read_excel('MEF.ods', engine='odf')
-    
-    main=True
-    options = [['Sim',True],['Não',False]]
-    while main:
-        question = [inquirer.List('prompt',message="Deseja continuar?",choices=[options[0][0],options[1][0]])]
-        main = options[[i[0] for i in options].index(inquirer.prompt(question)['prompt'])][1]
-        
-        if not main:
-            break
-        
-        initial_state = input('Initial state:')
-        input_string = input('Input string:')
-
-        #M: (S,input_string,alfa,initial_state,exit_state,transitions)
-        MEF = Finite_State_Machine(states,input_string,initial_state,transition_function)
-        MEF.execute()
-        
+    #M: (S,input_string,alfa,initial_state,exit_state,transitions)
+    MEF = Finite_State_Machine(states,transition_function)
+    MEF.start()    
